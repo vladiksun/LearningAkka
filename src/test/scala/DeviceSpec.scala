4465dd29ca54e5b645964967cbba679943ef04a2
Bondarchuk, Vladislav <VBondarchuk@nexign-systems.com>
2019-03-24 19:44:55 +0300

    commit

--- src/test/scala/DeviceSpec.scala
+++ src/test/scala/DeviceSpec.scala
@@ -1,7 +1,12 @@
-import akka.actor.ActorSystem
+import java.util.concurrent.TimeUnit
+
+import akka.actor.{ActorSystem, PoisonPill}
 import akka.testkit.{TestKit, TestProbe}
-import com.vb.iot.device.Device
+import com.vb.iot.actors.{Device, DeviceGroup, DeviceManager}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import scala.concurrent.duration._
+
+import scala.concurrent.duration.FiniteDuration
 
 class DeviceSpec(_system: ActorSystem)
 		extends TestKit(_system)
@@ -26,5 +31,130 @@ class DeviceSpec(_system: ActorSystem)
 
 		response.requestId should ===(42L)
 		response.value should ===(None)
+	}
+
+
+	"reply with latest temperature reading" in {
+		val probe = TestProbe()
+		val deviceActor = system.actorOf(Device.props("group", "device"))
+
+		deviceActor.tell(Device.RecordTemperature(requestId = 1, 24.0), probe.ref)
+		probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+
+		deviceActor.tell(Device.ReadTemperature(requestId = 2), probe.ref)
+		val response1 = probe.expectMsgType[Device.RespondTemperature]
+		response1.requestId should ===(2L)
+		response1.value should ===(Some(24.0))
+
+		deviceActor.tell(Device.RecordTemperature(requestId = 3, 55.0), probe.ref)
+		probe.expectMsg(Device.TemperatureRecorded(requestId = 3))
+
+		deviceActor.tell(Device.ReadTemperature(requestId = 4), probe.ref)
+		val response2 = probe.expectMsgType[Device.RespondTemperature]
+		response2.requestId should ===(4L)
+		response2.value should ===(Some(55.0))
+	}
+
+	"reply to registration requests" in {
+		val probe = TestProbe()
+		val deviceActor = system.actorOf(Device.props("group", "device"))
+
+		deviceActor.tell(DeviceManager.RequestTrackDevice("group", "device"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+		probe.lastSender should ===(deviceActor)
+	}
+
+	"ignore wrong registration requests" in {
+		val probe = TestProbe()
+		val deviceActor = system.actorOf(Device.props("group", "device"))
+
+		deviceActor.tell(DeviceManager.RequestTrackDevice("wrongGroup", "device"), probe.ref)
+		probe.expectNoMessage(500.milliseconds)
+
+		deviceActor.tell(DeviceManager.RequestTrackDevice("group", "Wrongdevice"), probe.ref)
+		probe.expectNoMessage(500.milliseconds)
+	}
+
+	"be able to register a device actor" in {
+		val probe = TestProbe()
+		val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+		val deviceActor1 = probe.lastSender
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("group", "device2"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+		val deviceActor2 = probe.lastSender
+		deviceActor1 should !==(deviceActor2)
+
+		// Check that the device actors are working
+		deviceActor1.tell(Device.RecordTemperature(requestId = 0, 1.0), probe.ref)
+		probe.expectMsg(Device.TemperatureRecorded(requestId = 0))
+		deviceActor2.tell(Device.RecordTemperature(requestId = 1, 2.0), probe.ref)
+		probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+	}
+
+	"ignore requests for wrong groupId" in {
+		val probe = TestProbe()
+		val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("wrongGroup", "device1"), probe.ref)
+		probe.expectNoMessage(500.milliseconds)
+	}
+
+	"return same actor for same deviceId" in {
+		val probe = TestProbe()
+		val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+		val deviceActor1 = probe.lastSender
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+		val deviceActor2 = probe.lastSender
+
+		deviceActor1 should ===(deviceActor2)
+	}
+
+	"be able to list active devices" in {
+		val probe = TestProbe()
+		val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("group", "device2"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+
+		groupActor.tell(DeviceGroup.RequestDeviceList(requestId = 0), probe.ref)
+		probe.expectMsg(DeviceGroup.ReplyDeviceList(requestId = 0, Set("device1", "device2")))
+	}
+
+	"be able to list active devices after one shuts down" in {
+		val probe = TestProbe()
+		val groupActor = system.actorOf(DeviceGroup.props("group"))
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("group", "device1"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+		val toShutDown = probe.lastSender
+
+		groupActor.tell(DeviceManager.RequestTrackDevice("group", "device2"), probe.ref)
+		probe.expectMsg(DeviceManager.DeviceRegistered)
+
+		groupActor.tell(DeviceGroup.RequestDeviceList(requestId = 0), probe.ref)
+		probe.expectMsg(DeviceGroup.ReplyDeviceList(requestId = 0, Set("device1", "device2")))
+
+		probe.watch(toShutDown)
+		toShutDown ! PoisonPill
+		probe.expectTerminated(toShutDown)
+
+		// using awaitAssert to retry because it might take longer for the groupActor
+		// to see the Terminated, that order is undefined
+		probe.awaitAssert {
+			groupActor.tell(DeviceGroup.RequestDeviceList(requestId = 1), probe.ref)
+			probe.expectMsg(DeviceGroup.ReplyDeviceList(requestId = 1, Set("device2")))
+		}
 	}
 }
